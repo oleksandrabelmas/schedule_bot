8ff42fd6e53b9b8cba7b7b863ad823f1534b4920
oleksandrabelmas <oleksandr.abelmas@gmail.com>
2023-02-27 00:35:15 +0200

    зробив клавіатури для вводу класу, все працює

--- handlers.py
+++ handlers.py
@@ -13,12 +13,18 @@ SHEET_ID = '165FEfvPuH9CfYK0qLp1Xa6o35aTW7WKdOmy5eqEvmWw'
 SHEET_NAME = 'Розклад 20.02.2023 по 24.02.2023'
 
 
+# решаю как бидло через глобальную переменую
+user_class_number = []
+
+
 async def class_num(message: types.Message, state: FSMContext):
     await message.answer(f'Привіт {message.from_user.first_name}, вибери номер класу', reply_markup=class_number_kb)
     await MyStates.choose_class_letter.set()
 
 
 async def choose_class(message: types.Message, state: FSMContext):
+    global user_class_number
+    user_class_number.append(message.text)
 
     # створюємо клавіатуру тут бо єбу як зробить не тут
     kb = ReplyKeyboardMarkup(
@@ -35,56 +41,50 @@ async def choose_class(message: types.Message, state: FSMContext):
 
 
 async def schedule(message: types.Message, state: FSMContext):
+    global user_class_number
+    user_class_number += message.text
+
+    user_class_number = '-'.join(user_class_number)
 
     get_data(SHEET_ID, SHEET_NAME)
 
     with open('data.json', 'r') as f:
         schdl = json.load(f)
 
-    user_message = '-'.join(message.text.split())
-    print(user_message)
     class_numbers = []
 
     for class_number, class_schedule in schdl.items():
         class_numbers.append(class_number)
 
-    if user_message in class_numbers:
-        await message.answer('Тепер ви будете отримувати сповіщення про уроки', reply_markup=cancel)
+    await message.answer('Тепер ви будете отримувати сповіщення про уроки', reply_markup=cancel)
 
-        for class_number, class_schedule in schdl.items():
+    for class_number, class_schedule in schdl.items():
 
-            if class_number == user_message:
+        if class_number == user_class_number:
 
-                # день тижня зараз
-                today_is = time.asctime().split()[0]
+            # день тижня зараз
+            today_is = time.asctime().split()[0]
 
-                for day, list_lessons in class_schedule.items():
-                    if day == today_is:
-                        while user_message != 'Припинити нагадування':
+            for day, list_lessons in class_schedule.items():
 
-                            # час зараз у потрібному форматі
-                            time_now = time.asctime().split()[3].split(':')
-                            time_now.pop(2)
-                            time_now = ':'.join(time_now)
+                if day == today_is:
+                    while True:
 
-                            # відправляю повідомлення
-                            for lesson in list_lessons:
-                                for lesson_time, lesson_name in lesson.items():
-                                    if lesson_time == time_now:
-                                        print(lesson_name, time_now)
-                                        if lesson_name != '':
-                                            await message.answer(lesson_name)
-                                            time.sleep(60)
+                        # час зараз у потрібному форматі
+                        time_now = time.asctime().split()[3].split(':')
+                        time_now.pop(2)
+                        time_now = ':'.join(time_now)
 
-                            else:
-                                await message.answer('OK')
+                        # відправляю повідомлення
+                        for lesson in list_lessons:
+                            for lesson_time, lesson_name in lesson.items():
 
-    elif message.text == 'Припинити нагадування':
-        await message.answer('OK')
-        await MyStates.menu.set()
+                                if lesson_time == time_now:
+                                    print(lesson_name, time_now)
 
-    else:
-        await message.answer('чьооо, я не знаю такого класу, надішліть ще раз')
+                                    if lesson_name != '':
+                                        await message.answer(lesson_name)
+                                        time.sleep(60)
 
 
 #async def menu(dp: Dispatcher, state: FSMContext):
--- tests.py
+++ tests.py
@@ -1,13 +1,4 @@
-import json
+l = []
 
-
-with open('data.json', 'r') as f:
-    schdl = json.load(f)
-
-class_numbers = []
-
-for class_number, class_schedule in schdl.items():
-    class_numbers.append(class_number)
-print(class_numbers)
-
-
+l += ['jlkfdjkfsdjklsfd']
+print(l)
